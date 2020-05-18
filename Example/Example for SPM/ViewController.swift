29fc6f756a6b8694fc762bd38163d45cad320d33
sonifex <sonerguler93@gmail.com>
2020-05-18 16:17:32 +0300

    Podspec localizable fix

---
+++ Example/Example for SPM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example for SPM
+//
+//  Created by Soner Güler on 18.05.2020.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
